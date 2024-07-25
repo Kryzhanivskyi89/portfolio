b3c763310857b907592450a3b94f4e64a2f5eb45
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2024-07-25 07:47:26 +0300

    change images

---
+++ about.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+        const aboutSections = document.querySelectorAll('.about-description');
+
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('visible');
+                } else {
+                    entry.target.classList.remove('visible');
+                }
+            });
+        }, { threshold: 0.1 });
+
+        aboutSections.forEach(section => {
+            observer.observe(section);
+        });
+    });
